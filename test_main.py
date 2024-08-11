3242c20713b8567a427860029eec53eb7605a69f
hesham1salem <h.salem@wakeb.tech>
2024-08-11 18:54:10 +0300

    add deploy

--- test_main.py
+++ test_main.py
@@ -1,13 +1,5 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
-
-app = FastAPI()
-
-
-@app.get("/")
-async def read_main():
-    return {"msg": "Hello World"}
-
+from .main import app
 
 client = TestClient(app)
 
